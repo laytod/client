15dcd11b37abbe59bb626f52a3a950273e851719
Layton McDaniel <laytonmcdaniel@gmail.com>
2015-03-01 19:33:10 -0500

    Add sleep to camera

--- cameraPi/cam.py
+++ cameraPi/cam.py
@@ -27,64 +27,21 @@ camera.crop = (0.0, 0.0, 1.0, 1.0)
 # changes
 camera.vflip = True
 camera.hflip = True
+sleep_duration = 1
 
 if __name__ == '__main__':
-	# path = '/home/pi/flask/cameraPi/static/camera'
 
 	# write to shared memory (RAM) so we don't wear out the SD card
 	path = '/run/shm/'
-	count = 0
 
 	print 'Taking pictures and saving them to {path}/image.jpg'.format(path=path)
 	print 'press ^C to quit.'
 
 	cmd = """convert -font helvetica -pointsize 20 -fill black -draw "text 430,470 '$(date)'" {path}tmp.jpg {path}image.jpg
 	""".format(path=path)
+
 	# loop forever taking pictures
 	while True:
-		# try:
-		print 'taking picture {num}'.format(num=count)
 		camera.capture('{path}/tmp.jpg'.format(path=path))
-		
-
-
 		subprocess.call(cmd, shell=True)
-
-		# subprocess.call('cp {path}/tmp.jpg {path}/image.jpg'.format(path=path), shell=True)
-		count += 1
-		# except Exception:
-		# 	camera.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		time.sleep(sleep_duration)
