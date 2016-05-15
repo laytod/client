d5ba970a44c5300256f34bf5f71806caf2cfa8b3
Layton McDaniel <laytonmcdaniel@gmail.com>
2016-05-15 12:29:40 -0400

    Add task status endpoint

--- cameraPi/views/pin.py
+++ cameraPi/views/pin.py
@@ -1,21 +1,24 @@
-from base import BaseView
+from flask import jsonify
 from flask.ext.classy import route
+
+from base import BaseView
 from cameraPi import pin_manager
 
 
 class PinView(BaseView):
     @route('/status/<pin_id>')
     def status(self, pin_id):
-        return pin_manager.get_info()
+        results = pin_manager.get_info()
+        return jsonify(results=results)
 
     @route('/toggle/<pin_id>')
     def toggle(self, pin_id):
-        return 'toggle pin {}'.format(pin_id)
+        pin_manager.toggle(pin_id)
 
     @route('/start/<pin_id>')
     def start(self, pin_id):
-        return 'start pin {}'.format(pin_id)
+        pin_manager.start(pin_id)
 
     @route('/stop/<pin_id>')
     def stop(self, pin_id):
-        return 'stop pin {}'.format(pin_id)
+        pin_manager.stop(pin_id)
--- cameraPi/views/tasks.py
+++ cameraPi/views/tasks.py
@@ -1,6 +1,10 @@
+from flask import jsonify
+
 from base import BaseView
+from cameraPi import task_manager
 
 
 class AllView(BaseView):
     def status(self):
-        return 'all freakin statuses'
+        results = task_manager.get_info()
+        return jsonify(results=results)
