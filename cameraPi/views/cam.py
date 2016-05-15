e93588fc1dbe71f8909c71540a799652d023d8c2
Layton McDaniel <laytonmcdaniel@gmail.com>
2016-05-15 13:43:55 -0400

    Fix cam endpoint bugs

--- cameraPi/views/cam.py
+++ cameraPi/views/cam.py
@@ -6,11 +6,27 @@ from cameraPi import task_manager
 
 class CamView(BaseView):
     def status(self):
-        cam_status = task_manager.get_info('cam')
-        return jsonify(results=cam_status)
+        task_status = task_manager.get_info()
+        cam_state = False
+        mjpg_state = False
+
+        for task in task_status:
+            name = task['name']
+            if name == 'cam':
+                cam_state = task['statename'] != 'RUNNING'
+            elif name == 'mjpg':
+                mjpg_state = task['statename'] != 'RUNNING'
+
+        results = {
+            'name': 'cam',
+            'state': cam_state and mjpg_state,
+            'data': {}
+        }
+        return jsonify(results=results)
 
     def start(self):
         task_manager.start('cam')
+        task_manager.start('mjpg')
         results = {
             'name': 'cam',
             'state': True,
@@ -20,6 +36,7 @@ class CamView(BaseView):
 
     def stop(self):
         task_manager.stop('cam')
+        task_manager.stop('mjpg')
         results = {
             'name': 'cam',
             'state': False,
