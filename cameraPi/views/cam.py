62d78c0c5a8ce01cd0b18c8bf4e84f30226b22ca
Layton McDaniel <laytonmcdaniel@gmail.com>
2016-05-15 13:34:18 -0400

    Add endpoints for cam

--- cameraPi/views/cam.py
+++ cameraPi/views/cam.py
@@ -1,9 +1,28 @@
+from flask import jsonify
+
 from base import BaseView
+from cameraPi import task_manager
 
 
 class CamView(BaseView):
     def status(self):
-        return 'cam status'
+        cam_status = task_manager.get_info('cam')
+        return jsonify(results=cam_status)
 
-    def toggle(self):
-        return 'cam toggle'
+    def start(self):
+        task_manager.start('cam')
+        results = {
+            'name': 'cam',
+            'state': True,
+            'success': True,
+        }
+        return jsonify(results=results)
+
+    def stop(self):
+        task_manager.stop('cam')
+        results = {
+            'name': 'cam',
+            'state': False,
+            'success': True,
+        }
+        return jsonify(results=results)
--- cameraPi/views/tasks.py
+++ cameraPi/views/tasks.py
@@ -1,10 +1,11 @@
 from flask import jsonify
 
 from base import BaseView
-from cameraPi import task_manager
+from cameraPi import task_manager, pin_manager
 
 
 class AllView(BaseView):
     def status(self):
-        results = task_manager.get_info()
-        return jsonify(results=results)
+        pin_results = pin_manager.get_info()
+        task_results = task_manager.get_info()
+        return jsonify(results=[pin_results, task_results])
